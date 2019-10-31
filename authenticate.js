5a1fce2495b300eaf33b8b0264f96838bb1da46b
Gapur Kassym <gapur.kassym@gmail.com>
2019-11-01 00:05:25 +0600

    feat(authenticate): impelemt verifyAdmin

--- authenticate.js
+++ authenticate.js
@@ -33,3 +33,12 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
   }));
 
 exports.verifyUser = passport.authenticate('jwt', { session: false });
+
+exports.verifyAdmin = (req, res, next) => {
+  if (req.user && req.user.admin) {
+    return next();
+  }
+  const err = new Error('You are not authorized to perform this operation!');
+  err.status = 403;
+  return next(err);
+};
