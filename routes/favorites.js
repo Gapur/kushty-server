6e5723246e158ba16a030283b84aea64c55d0bc6
Gapur Kassym <gapur.kassym@gmail.com>
2019-11-04 10:51:28 +0600

    refactor(favorites): post endpoint

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
 
 const Favorites = require('../models/favorites');
 const authenticate = require('../authenticate');
 
 const router = express.Router();
-const { ObjectId } = mongoose.Types;
 
 router.get('/', (req, res, next) => {
   Favorites.findOne({ user: req.user._id })
@@ -19,13 +17,33 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', authenticate.verifyUser, (req, res, next) => {
-  const data = { dishes: req.body, user: req.user._id };
-  Favorites.create(data)
-    .then((favorites) => {
-      console.log('Favorites Created ', favorites);
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json(favorites);
+  Favorites.findOne({ user: req.user._id })
+    .then((favorite) => {
+      if (favorite == null) {
+        const data = { dishes: req.body, user: req.user._id };
+        Favorites.create(data)
+          .then((favorites) => {
+            console.log('Favorites Created ', favorites);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(favorites);
+          }, (err) => next(err));
+      } else {
+        const dishes = req.body.reduce((acc, curr) => (favorite.dishes.includes(curr._id)
+          ? acc : acc.concat(curr._id)), []);
+        if (dishes.length === 0) {
+          const err = new Error('Favorites should contain unique dishes');
+          return next(err);
+        }
+        favorite.dishes.push(dishes);
+        favorite.save()
+          .then((savedFavorites) => {
+            console.log('Added ', savedFavorites);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(savedFavorites);
+          }, (err) => next(err));
+      }
     }, (err) => next(err))
     .catch((err) => next(err));
 });
@@ -48,7 +66,7 @@ router.post('/:dishId', authenticate.verifyUser, (req, res, next) => {
         return next(err);
       }
 
-      if (favorite.dishes.some((id) => ObjectId(id).equals(req.params.dishId))) {
+      if (favorite.dishes.includes(req.params.dishId)) {
         const err = new Error(`Dish ${req.params.dishId} is already exist in favorites`);
         return next(err);
       }
@@ -76,7 +94,7 @@ router.delete('/:dishId', authenticate.verifyUser, (req, res, next) => {
         return next(err);
       }
 
-      if (!favorite.dishes.some((id) => ObjectId(id).equals(req.params.dishId))) {
+      if (!favorite.dishes.includes(req.params.dishId)) {
         const err = new Error(`You don't have ${req.params.dishId} in your favorites`);
         return next(err);
       }
